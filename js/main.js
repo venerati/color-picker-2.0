66619035027fafdd2441acfc5b8a17ae712d4330
James Marshall <jmarshall@mshop360.com>
2016-11-09 11:24:23 -0500

    created original documents needed for the site. planned out functions.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// place image onto the canvas
+
+// get the 2d position of the pointer on the canvas
+
+// sample the image for color on the pointer's location
+
+// inject hex value into box
+
+// inject rgb value into box
+
+// inject color into visual rep
