4aed2b04b787e65027d2b40766b5d36f36eb9b3c
James Marshall <jmarshall@mshop360.com>
2016-11-09 15:40:46 -0500

    built in on click function that pulls your mouse's xy position. then goes back and pulls the image data of that xy position.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,37 @@
-// setting a variable that selects the cavas but it's ID
+// setting a variable that selects the cavas buy it's ID and specifies that its 2d
 var canvas = document.getElementById('canvasPick'),
-
-// specifies that the object is 2d
-context = canvas.getContext('2d');
-
-make_base();
-
-//this function handles the creations and injection of the image to the canvas
-function make_base()
-{
-	//creates image code
-  base_image = new Image();
-  //selects image source and puts it into image code
-  base_image.src = 'colors.png';
-  //forces the function to wait for the image to load
-  base_image.onload = function(){
-  	//selects 2d object and injects image
-    context.drawImage(base_image, 0, 0);
-  }
-}
-
-
+ 
+ // specifies that the object is 2d
+ context = canvas.getContext('2d');
+ 
+ make_base();
+ 
+ //this function handles the creations and injection of the image to the canvas
+ function make_base()
+ {
+ 	//creates image code
+   base_image = new Image();
+   //selects image source and puts it into image code
+   base_image.src = 'colors.png';
+   //forces the function to wait for the image to load
+   base_image.onload = function(){
+   	//selects 2d object and injects image
+     context.drawImage(base_image, 0, 0);
+   }
+ }
 
 
+canvas.addEventListener('click', function(event) {
+    var x = event.pageX;
+        y = event.pageY;
+
+    var img_data = context.getImageData(x, y, 1, 1).data;
+	  var R = img_data[0];
+	  var G = img_data[1];
+	  var B = img_data[2];  var rgb = R + ',' + G + ',' + B;
+
+	  console.log(R);
+    });
 
 // sample the image for color on the pointer's location
 
@@ -33,3 +42,5 @@ function make_base()
 // inject color into visual rep
 
 // http://www.javascripter.net/faq/rgbtohex.htm
+
+	// http://www.javascripter.net/faq/rgbtohex.htm
