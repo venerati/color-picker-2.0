3d26fd5330ef7d09fc27e2cb68532076a5d87798
James Marshall <jmarshall@mshop360.com>
2016-11-09 12:46:42 -0500

    created the js function to inject the background image to the canvas

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-// place image onto the canvas
+// setting a variable that selects the cavas but it's ID
+var canvas = document.getElementById('canvasPick'),
 
-// get the 2d position of the pointer on the canvas
+// specifies that the object is 2d
+context = canvas.getContext('2d');
+
+make_base();
+
+//this function handles the creations and injection of the image to the canvas
+function make_base()
+{
+	//creates image code
+  base_image = new Image();
+  //selects image source and puts it into image code
+  base_image.src = 'colors.png';
+  //forces the function to wait for the image to load
+  base_image.onload = function(){
+  	//selects 2d object and injects image
+    context.drawImage(base_image, 0, 0);
+  }
+}
+
+
+
+
 
 // sample the image for color on the pointer's location
 
@@ -9,3 +31,5 @@
 // inject rgb value into box
 
 // inject color into visual rep
+
+// http://www.javascripter.net/faq/rgbtohex.htm
